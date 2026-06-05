0825a23abb6fbce29a8b51e75523f37bf773b347
Abin K Johny <abinkjohny2011@gmail.com>
2026-06-05 15:56:40 +0530

    fix: update import path for shouldRetry function in use-attachment hook

--- src/hooks/attachment/use-attachment.ts
+++ src/hooks/attachment/use-attachment.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { attachmentService } from '@echno-core/services/attachment-service';
 import { Attachment } from '@echno-core/types/attachment';
-import { shouldRetry } from '@tornotron/echno-core';
+import { shouldRetry } from '@echno-core/lib/query/retry';
 import { attachmentKeys } from './attachment-keys';
 
 /**
