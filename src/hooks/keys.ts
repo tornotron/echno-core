9e154dc9eba4b6531999419ad1bdbafbad79bf1b
Abin K Johny <abinkjohny2011@gmail.com>
2026-06-10 17:42:59 +0530

    feat: add index files for hooks, services, and types to export necessary modules

---
+++ src/hooks/keys.ts
@@ -0,0 +1,17 @@
+export {
+  attachmentKeys,
+  ATTACHMENT_PARENT_NAMESPACES,
+} from './attachment/attachment-keys';
+export { userKeys } from './user/user-keys';
+export { employeeKeys } from './employee/employee-keys';
+export { projectKeys } from './project/project-keys';
+export { taskKeys } from './task/task-keys';
+export { workCategoryKeys } from './work-category/work-category-keys';
+export { issueKeys, issueCommentKeys } from './issue/issue-keys';
+export { organizationKeys } from './organization/organization-keys';
+export { invitationKeys } from './invitation/invitation-keys';
+export { storageLocationKeys } from './storage-locations/storage-location-keys';
+export { labourKeys } from './labour/labour-keys';
+export { vendorKeys } from './vendor/vendor-keys';
+export { materialsKeys } from './materials/material-keys';
+export { materialConsumptionsKeys } from './material-consumption/material-consumption-keys';
