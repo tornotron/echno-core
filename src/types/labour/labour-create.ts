fee370b3eeb527239aa99ed1039ed6d510852afd
Abin K Johny <abinkjohny2011@gmail.com>
2026-06-10 13:13:51 +0530

    feat(types): add Labour, LabourCreateRequest, and LabourUpdateRequest interfaces with parsing functions

---
+++ src/types/labour/labour-create.ts
@@ -0,0 +1,23 @@
+import { EmploymentType, SkillLevel } from './enums';
+
+export interface LabourCreateRequest {
+  labourID?: string;
+  fullName?: string;
+  email?: string;
+  address?: string;
+  phoneNumber?: string;
+  emergencyContactName?: string;
+  emergencyContactPhone?: string;
+  specialization?: string;
+  employmentType?: EmploymentType;
+  skillLevel?: SkillLevel;
+  status?: string;
+  joiningDate: string;
+  currentProjectId?: number;
+  dailyRate?: number;
+  overTimeRate?: number;
+  bankAccountNumber?: string;
+  bankName?: string;
+  ifscCode?: string;
+  additionalNotes?: string;
+}
---
+++ src/types/labour/labour-update.ts
@@ -0,0 +1,23 @@
+import { EmploymentType, LabourStatus, SkillLevel } from './enums';
+
+export interface LabourUpdateRequest {
+  labourID?: string;
+  fullName?: string;
+  email?: string;
+  address?: string;
+  phoneNumber?: string;
+  emergencyContactName?: string;
+  emergencyContactPhone?: string;
+  specialization?: string;
+  employmentType?: EmploymentType;
+  skillLevel?: SkillLevel;
+  status?: LabourStatus;
+  joiningDate?: string;
+  currentProjectId?: number | null;
+  dailyRate?: number;
+  overTimeRate?: number;
+  bankAccountNumber?: string;
+  bankName?: string;
+  ifscCode?: string;
+  additionalNotes?: string;
+}
